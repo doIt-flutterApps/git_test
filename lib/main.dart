b532e2fd583e917dfc247197ff6270afe48556f4
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-04-17 10:53:31 +0900

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Commit 2',
+      title: 'Flutter Demo Commit v.2',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page v.1'),
     );
   }
 }

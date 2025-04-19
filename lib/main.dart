b6b60f6a6b946004664d97b58e485c1802910bd2
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-04-19 15:17:58 +0900

    commit 6 HEAD

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Commit v.2',
+      title: 'Flutter Demo Commit v.3',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
